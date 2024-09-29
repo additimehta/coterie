ff6c2ef1e1caec5d8899cb7962ad732bdccf1a19
Chloe Wei <wei.chloe55@gmail.com>
2024-09-28 23:06:25 -0400

    chat simulation implemented, chatbot code cleaned

--- pages/Chats.py
+++ pages/Chats.py
@@ -1,7 +1,21 @@
 import streamlit as st
 
-# Display a title
-st.title("MatchUP: Grow your network, one match at a time")
+import random
+import time
+
+st.title("Maria Smith")
+
+
+# Get question from user
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Display "connection" response in chat message container
+    with st.chat_message("assistant"):
+            st.markdown("Hi Emily! Nice to meet you! Of course, I'm happy to help!")
+
 
 with st.sidebar:
     titlecol1, titlecol2, titlecol3 = st.columns([1, 3, 1])  # Adjust the column widths as needed
